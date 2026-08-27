96c4db411fb4ff106f562751c5ec10386a2cca47
yhArcadia <1761869682@qq.com>
2026-08-28 00:21:24 +0800

    为锅巴配置界面增加分页

--- guoba/schemas/configuration.js
+++ guoba/schemas/configuration.js
@@ -1,7 +1,7 @@
 const labels = {
   linkedGroups: '互通群组',
   maxSaveLength: '单人保留记录上限',
-  maxRenderLength: 'kh卡最大渲染记录条数',
+  maxRenderLength: 'kh卡最大条数',
   autoUpdateGroups: '自动更新群',
   updateSchedule: '自动更新 Cron',
   notifyGroups: '实时推送群',
@@ -17,6 +17,7 @@ const labels = {
 
 export default [
 
+  { component: 'SOFT_GROUP_BEGIN', label: '常用设置' },
   { component: 'Divider', label: '基础设置' },
   { field: 'maxSaveLength', label: labels.maxSaveLength, component: 'InputNumber', componentProps: { min: 1, max: 200 }, bottomHelpMessage: '每个用户在单个群内最多保存的记录条数，超过这个值的最早记录将被丢弃。' },
   { field: 'maxRenderLength', label: labels.maxRenderLength, component: 'InputNumber', componentProps: { min: 1, max: 100 }, bottomHelpMessage: '普通开合卡显示最近几条；大写KH会绕过此限制从而渲染完整的全部记录。' },
@@ -47,21 +48,7 @@ export default [
       `
   },
 
-
-
-  { component: 'Divider', label: '自动更新与定时任务' },
-  { field: 'updateSchedule', label: labels.updateSchedule, component: 'Input', bottomHelpMessage: '定时更新 Cron 表达式，例如 0 30 3 * * * 表示每天凌晨 03:30执行一次。保存后重启 Yunzai 使配置和调度器生效。' },
-  {
-    field: 'autoUpdateGroups',
-    label: labels.autoUpdateGroups,
-    component: 'GSelectGroup',
-    componentProps: { placeholder: `请选择要启用自动更新的群` },
-    bottomHelpMessage: `插件会在上方Cron指定的时间，自动更新这些群的全部群员的信息`
-  },
-
-
-
-  { component: 'Divider', label: '黑白名单：插件默认对全部群的全部成员生效，可修改下方三个名单来控制范围。' },
+  { component: 'SOFT_GROUP_BEGIN', label: '生效范围' },
   {
     field: 'userBlacklist',
     label: labels.userBlacklist,
@@ -92,10 +79,6 @@ export default [
     componentProps: { placeholder: `选择黑名单群` },
     bottomHelpMessage: `黑名单中的群，插件始终不更新其任何群员的信息。仅在【群白名单为空】时，群黑名单才生效。注意：主人依旧可以使用 更新群员信息 或 静默更新信息 来手动更新群员（黑名单用户除外）的信息。`
   },
-
-
-
-  { component: 'Divider', label: '范围与权限' },
   {
     field: 'divingGroups',
     label: labels.divingGroups,
@@ -103,6 +86,19 @@ export default [
     componentProps: { placeholder: `请选择潜水群` },
     bottomHelpMessage: `在这些群中，依然可正常记录与更新群员的信息，但无法触发本插件的任何文本或图片响应，以此避免暴露Bot身份。\n主人在潜水群发送“更新群员信息”会转为执行“静默更新信息”。`
   },
+
+
+  { component: 'SOFT_GROUP_BEGIN', label: '进阶配置' },
+  { component: 'Divider', label: '自动更新与定时任务' },
+  { field: 'updateSchedule', label: labels.updateSchedule, component: 'Input', bottomHelpMessage: '定时更新 Cron 表达式，例如 0 30 3 * * * 表示每天凌晨 03:30执行一次。保存后重启 Yunzai 使配置和调度器生效。' },
+  {
+    field: 'autoUpdateGroups',
+    label: labels.autoUpdateGroups,
+    component: 'GSelectGroup',
+    componentProps: { placeholder: `请选择要启用自动更新的群` },
+    bottomHelpMessage: `插件会在上方Cron指定的时间，自动更新这些群的全部群员的信息`
+  },
+  { component: 'Divider', label: '互通组配置' },
   {
     field: 'linkedGroups',
     label: labels.linkedGroups,
